9f98bac6e0d13a5043173ff35fa831ff149b05df
jsimpsonux <jsimpsonux@gmail.com>
2024-01-30 23:40:43 +0000

    Added the README file and made file structure changes

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ $( document ).ready(function() {
 
     
         var apiKey = "09b0cfac23642f5c80a049c5ee5b97c4";
-        var searchInput = $('#search-input').val().trim();
-        var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
+        // var searchInput = $('#search-input').val().trim();
+        // var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
         // var queryURL = " http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
 
         var currentDate = $('#today');
@@ -11,6 +11,10 @@ $( document ).ready(function() {
     
         $("#search-button").on("click", function (event) {
             event.preventDefault()
+
+            var searchInput = $('#search-input').val().trim();
+        var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
+
             fetch(queryURL)
             .then(function (response) {
                 return response.json();
