c5b3d8b948637afbc3d22bf7aa1f94221258b9c3
jsimpsonux <jsimpsonux@gmail.com>
2024-02-26 21:39:56 +0000

    Made changes to index.js and index.html

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+var apiKey = "09b0cfac23642f5c80a049c5ee5b97c4";
+
 $( document ).ready(function() {
 
     
-        var apiKey = "09b0cfac23642f5c80a049c5ee5b97c4";
         // var searchInput = $('#search-input').val().trim();
         // var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
         // var queryURL = " http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
@@ -13,7 +14,8 @@ $( document ).ready(function() {
             event.preventDefault()
 
             var searchInput = $('#search-input').val().trim();
-        var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
+            var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=5&appid=" + apiKey;
+
 
             fetch(queryURL)
             .then(function (response) {
@@ -21,15 +23,7 @@ $( document ).ready(function() {
             }).then(function (data) {
                 console.log(data);
 
-                fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=metric" + "&appid=" + apiKey)
-                .then(function (response) {
-                    return response.json();
-                }).then(function (data) {
-                    console.log(data);
-
-
-
-                })
+                currentWeather(data[0].lat, data[0].lon)
 
                 fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=metric" + "&appid=" + apiKey)
                 .then(function (response) {
@@ -37,20 +31,66 @@ $( document ).ready(function() {
                 }).then(function (data) {
                     console.log(data);
 
+
+
                     for (var i = 0; i < data.list.length; i += 8) {
+
+                        var cardStyle = $("<div>").addClass("card").css("width: 18rem;");
+                        var cardBody = cardStyle.prepend("<div class='card'></div>");
 
                         var foreCast = $("<p>").addClass("font-weight-bold text-primary").text(data.list[i].main.temp);
                         var showWind = $("<p>").text(data.list[i].wind.speed);
                         var showHumidity = $("<p>").text(data.list[i].main.humidity);
-                        var today = dayjs().format('L');
                         var thisDate = currentDate.text(data.list[i].dt_text);
+                        var columns = $("<div class='col'></div>");
+
+                        var cardContent = cardBody.prepend(columns)
+
+                        columns.append(foreCast, showWind, showHumidity, thisDate);
+
         
-                        $("#forecast").append(foreCast, showWind, showHumidity, thisDate);
+                        $("#forecast").append(columns);
                     }
+
+
+                    
 
                 });
 
 
             });
+
+            // display here
+            var storedCities = [];
+
+            // if (var i = 0; i < data.name.length; i++) {
+
+            // }
+
+            
+
         });
 });
+
+function currentWeather(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric" + "&appid=" + apiKey)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+
+        var foreCast = $("<p>").addClass("font-weight-bold text-primary").text(data.main.temp);
+        var showWind = $("<p>").text(data.wind.speed);
+        var showHumidity = $("<p>").text(data.main.humidity);
+        var showPlace = $("<h1>").text(data.name);
+        // var thisDate = currentDate.text(data.list.dt_text);
+        var today = dayjs().format('DD/MM/YYYY');
+
+        $("#today").append(showPlace, today, foreCast, showWind, showHumidity)
+
+
+
+    })
+
+
+}
